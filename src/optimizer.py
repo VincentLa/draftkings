99a19e0af64f2985a8d9500e5f2d15f70d6c0f75
KGe001 <69283269+KGe001@users.noreply.github.com>
2020-08-06 22:46:14 -0400

    20200806_updates
    - Using box_scores + salaries, built first version of mappingtable
    - Added injury reports from nba.com
    https://official.nba.com/nba-injury-report-2019-20-season/

--- src/optimizer.py
+++ src/optimizer.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy
 from pulp import LpMaximize, LpProblem, LpStatus, lpSum, LpVariable
-
-model = LpProblem(name='dfs-nba', sense=LpMaximize)
+import os
 
 # import dfs stats and set up variable universe:
-df = pd.read_csv(r'data\raw\draftkings_salaries\DKNBASalaries_20200806.csv')
+df = pd.read_csv(r'data\raw\draftkings_salaries\DKNBASalaries_Classic_20200806.csv')
 df['PG'] = df['Roster Position'].str.contains('PG')
 df['SG'] = df['Roster Position'].str.contains('SG')
 df['SF'] = df['Roster Position'].str.contains('SF')
@@ -15,6 +14,31 @@ df['G'] = df['Roster Position'].str.contains('G')
 df['F'] = df['Roster Position'].str.contains('F')
 df['UTIL'] = df['Roster Position'].str.contains('UTIL')
 
+### Testing with boxscores
+# import nba_box_scores and avg
+dkpts_df = pd.DataFrame()
+for file in os.listdir(r'data\raw\nba_box_score_stats'):
+    df_box = pd.read_csv(r'data\raw\nba_box_score_stats' + '\\' + file)[['slug', 'name', 'date_played', 'draftkings_points']]
+    dkpts_df = dkpts_df.append(df_box)
+
+dkpts_df_avg = dkpts_df[['name', 'draftkings_points']].groupby('name').mean()
+
+# # mapping table
+# dkmap = dkpts_df[['slug', 'name']].drop_duplicates()
+# dkmap2 = df[['ID', 'Name']].drop_duplicates()
+# mappingtable = dkmap2.merge(dkmap, left_on='Name', right_on='name', how='outer')
+# mappingtable.to_csv('mappingtable.csv')
+
+mapping_df = pd.read_csv('mappingtable.csv', index_col=0)[['ID', 'slug', 'name']]
+test = df.merge(mapping_df, on='ID', how='left')
+test = test.merge(dkpts_df_avg, on='name', how='left')
+test = test.drop('AvgPointsPerGame', axis=1)
+test = test.rename(columns={'draftkings_points':'AvgPointsPerGame'})
+test.AvgPointsPerGame = test.AvgPointsPerGame.fillna(0)
+
+df = test
+
+model = LpProblem(name='dfs-nba', sense=LpMaximize)
 player_wgts = LpVariable.dicts("Player",df.Name,lowBound=0,upBound=1,cat='Integer')
 
 # Add obj function
