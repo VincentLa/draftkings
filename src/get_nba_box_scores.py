8b4e47ca5b67a4c033b6c35c091819a19d5c94d2
Vincent La <vincela14@gmail.com>
2020-08-08 18:08:05 -0600

    Adding new command line script

---
+++ src/get_nba_box_scores.py
@@ -0,0 +1,193 @@
+import datetime as dt
+import os
+from pathlib import Path
+import sys
+
+import basketball_reference_web_scraper
+from basketball_reference_web_scraper import client
+import git
+import pandas as pd
+
+import src.util as ut
+
+
+GIT_ROOT_DIR = ut.get_git_root(os.getcwd())
+DATA_DIR = os.path.join(GIT_ROOT_DIR, 'data')
+
+# Constants to translate raw NBA stats to DraftKings Points.
+# Pulling them out here in case they change it's easy to swap out
+POINTS_MULTIPLE = 1
+THREE_POINT_MULTIPLE = 0.5
+REBOUND_MULTIPLE = 1.25
+ASSIST_MULTIPLE = 1.5
+STEAL_MULTIPLE = 2
+BLOCK_MULTIPLE = 2
+TURNOVER_MULTIPLE = -0.5
+DOUBLE_DOUBLE_MULTIPLE = 1.5
+TRIPLE_DOUBLE_MULTIPLE = 3
+
+HELP_TEXT = """
+This is a runner to get NBA box score data. We are largely depending on the following package: https://jaebradley.github.io/basketball_reference_web_scraper/api/.
+
+You need to specify a range, if you only want one day of data, you can just specify the same date for both the start and end date
+"""
+
+
+def get_args():
+    """Use argparse to parse command line arguments."""
+    parser = argparse.ArgumentParser(description=HELP_TEXT)
+    parser.add_argument(
+    	'--start_game_year',
+    	help='Start Year Range',
+        type=int,
+        required=True
+    )
+    parser.add_argument(
+        '--start_game_month',
+        help='Start Month Range',
+        type=int,
+        required=True
+
+    )
+    parser.add_argument(
+        '--start_game_day',
+        help='Start Day Range',
+        type=int,
+        required=True
+    )
+    parser.add_argument(
+    	'--end_game_year',
+    	help='End Year Range',
+        type=int,
+        required=True
+    )
+    parser.add_argument(
+        '--end_game_month',
+        help='End Month Range',
+        type=int,
+        required=True
+
+    )
+    parser.add_argument(
+        '--end_game_day',
+        help='End Day Range',
+        type=int,
+        required=True
+    )
+    return parser.parse_args()
+
+
+def get_player_box_scores_day(game_year, game_month, game_day):
+    """
+    Get all player box scores for a given day. Scraped from Basketball Reference
+    
+    Keyword Args:
+      - game_year: integer, year of game
+      - game_month: integer, month of game
+      - game_day: integer, day of game
+      
+    We are using a package: https://jaebradley.github.io/basketball_reference_web_scraper/api/
+    """
+    player_box_scores = client.player_box_scores(day=game_day, month=game_month, year=game_year)
+    
+    if len(player_box_scores) == 0:
+        print('There were no games played on date: {}-{}-{}'.format(
+        	game_year, str(game_month).rjust(2, '0'), str(game_day).rjust(2, '0')))
+        return None
+    
+    player_box_scores_df = pd.DataFrame(player_box_scores)
+    player_box_scores_df['date_played'] = dt.date(game_year, game_month, game_day)
+    player_box_scores_df['total_rebounds'] = player_box_scores_df['offensive_rebounds'] + player_box_scores_df['defensive_rebounds']
+    player_box_scores_df['total_points_scored'] = (player_box_scores_df['made_field_goals'] - player_box_scores_df['made_three_point_field_goals']) * 2 \
+        + player_box_scores_df['made_three_point_field_goals'] * 3 \
+        + player_box_scores_df['made_free_throws']
+    player_box_scores_df['total_double_digit_stats'] = (player_box_scores_df['total_points_scored'] >= 10).astype(int) \
+        + (player_box_scores_df['total_rebounds'] >= 10).astype(int) \
+        + (player_box_scores_df['assists'] >= 10).astype(int) \
+        + (player_box_scores_df['steals'] >= 10).astype(int) \
+        + (player_box_scores_df['blocks'] >= 10).astype(int)
+    player_box_scores_df['double_double_flag'] = (player_box_scores_df['total_double_digit_stats'] == 2).astype(int)
+    player_box_scores_df['triple_double_flag'] = (player_box_scores_df['total_double_digit_stats'] >= 3).astype(int)
+    
+    player_box_scores_df['draftkings_points'] = \
+        POINTS_MULTIPLE * player_box_scores_df['total_points_scored'] \
+        + THREE_POINT_MULTIPLE * player_box_scores_df['made_three_point_field_goals'] \
+        + REBOUND_MULTIPLE * player_box_scores_df['total_rebounds'] \
+        + ASSIST_MULTIPLE * player_box_scores_df['assists'] \
+        + STEAL_MULTIPLE * player_box_scores_df['steals'] \
+        + BLOCK_MULTIPLE * player_box_scores_df['blocks'] \
+        + TURNOVER_MULTIPLE * player_box_scores_df['turnovers'] \
+        + DOUBLE_DOUBLE_MULTIPLE * player_box_scores_df['double_double_flag'] \
+        + TRIPLE_DOUBLE_MULTIPLE * player_box_scores_df['triple_double_flag']
+    
+    player_box_scores_df = player_box_scores_df[[
+        'slug',
+        'name',
+        'team',
+        'location',
+        'opponent',
+        'date_played',
+        'outcome',
+        'seconds_played',
+        'made_field_goals',
+        'attempted_field_goals',
+        'made_three_point_field_goals',
+        'attempted_three_point_field_goals',
+        'made_free_throws',
+        'attempted_free_throws',
+        'total_points_scored',
+        'total_rebounds',
+        'assists',
+        'steals',
+        'blocks',
+        'turnovers',
+        'double_double_flag',
+        'triple_double_flag',
+        'draftkings_points',    
+    ]]
+    
+    player_box_scores_df.sort_values('draftkings_points', ascending=False, inplace=True)
+    
+    filename = os.path.join(
+        DATA_DIR, 'raw', 'nba_box_score_stats', 'nba_box_score_stats_{}{}{}.csv'.format(
+        	game_year, str(game_month).rjust(2, '0'), str(game_day).rjust(2, '0')))
+    player_box_scores_df.to_csv(filename, index=False, encoding='utf-8')
+    
+    return player_box_scores_df
+    
+
+def main():
+	"""
+	Run Getting NBA Box Scores
+	"""
+    args = get_args()
+    start_game_year = args.start_game_year
+    start_game_month = args.start_game_month
+    start_game_day = args.start_game_day
+    end_game_year = args.end_game_year
+    end_game_month = args.end_game_month
+    end_game_day = args.end_game_day
+
+    start_date = dt.date(start_game_year, start_game_month, start_game_day)
+    end_date = dt.date(end_game_year, end_game_month, end_game_day)
+
+    assert start_date <= end_date, 'Start Date is after End Date. Please enter a valid date range'
+
+    day_delta = dt.timedelta(days=1)
+
+    while start_date <= end_date:
+    	"""Get all box scores within the date range"""
+    	game_year = start_date.year
+    	game_month = start_date.month
+    	game_day = start_date.day
+
+    	player_box_scores_df = get_player_box_scores_day(
+    		game_year=game_year, game_month=game_month, game_day=game_day)
+
+    	start_date += day_delta
+
+
+ if __name__ == '__main__':
+    """See https://stackoverflow.com/questions/419163/what-does-if-name-main-do"""
+    main()
+
--- src/util.py
+++ src/util.py
@@ -1,17 +1,37 @@
+"""
+Save misc tools/functions here that are useful to have
+"""
+
 import os
 import uuid
 
+import git
 import pandas as pd
 import fuzzywuzzy
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 
 
-# save misc tools/functions here that are useful to have
+def get_git_root(path):
+    """Return Top Level Git Repository directory given path"""
+    git_repo = git.Repo(path, search_parent_directories=True)
+    git_root = git_repo.git.rev_parse("--show-toplevel")
+    return git_root
 
-# fuzzy merge for building mapping tables
+
+def check_missing_values(data):
+    """Checks Missing Values By Column in a DataFrame"""
+    df_count_missing = pd.DataFrame({
+        'number_total_rows': data.shape[0],
+        'number_missing_rows': data.isnull().sum(),
+        'percentage': data.isnull().mean()})
+    return df_count_missing
+
+
 def fuzzy_merge(df_1, df_2, key1, key2, threshold=80, limit=1):
     """
+    Fuzzy merge for building mapping tables
+
     :param df_1: the left table to join
     :param df_2: the right table to join
     :param key1: key column of the left table
@@ -30,34 +50,39 @@ def fuzzy_merge(df_1, df_2, key1, key2, threshold=80, limit=1):
 
     return df_1
 
-# get mapping
 
-# pull all box scores
-dkpts_df = pd.DataFrame()
-for file in os.listdir(r'data\raw\nba_box_score_stats'):
-    print(file)
-    df_box = pd.read_csv(r'data\raw\nba_box_score_stats' + '\\' + file, encoding='utf-8')[['slug', 'name', 'date_played', 'draftkings_points']]
-    dkpts_df = dkpts_df.append(df_box)
+def get_mapping():
+    """
+    Get Mapping
 
-# pull salaries
-sal_df = pd.DataFrame()
-for file in os.listdir(r'data\raw\draftkings_salaries'):
-    print(file)
-    df_s = pd.read_csv(r'data\raw\draftkings_salaries' + '\\' + file)
-    sal_df = sal_df.append(df_s)
+    @Kevin -- if you can clean this up
+    """
+    # pull all box scores
+    dkpts_df = pd.DataFrame()
+    for file in os.listdir(r'data\raw\nba_box_score_stats'):
+        print(file)
+        df_box = pd.read_csv(r'data\raw\nba_box_score_stats' + '\\' + file, encoding='utf-8')[['slug', 'name', 'date_played', 'draftkings_points']]
+        dkpts_df = dkpts_df.append(df_box)
 
-# mapping table
-dkmap = dkpts_df[['slug', 'name']].drop_duplicates()
-dkmap2 = sal_df[['ID', 'Name']].drop_duplicates()
-mappingtable = fuzzy_merge(dkmap2, dkmap, key1='Name', key2='name')
-mappingtable.drop('ID', axis=1, inplace=True)
-mappingtable = mappingtable.drop_duplicates()
-test = mappingtable.merge(dkmap, left_on='matches', right_on='name', how='left')
+    # pull salaries
+    sal_df = pd.DataFrame()
+    for file in os.listdir(r'data\raw\draftkings_salaries'):
+        print(file)
+        df_s = pd.read_csv(r'data\raw\draftkings_salaries' + '\\' + file)
+        sal_df = sal_df.append(df_s)
 
-for i in test.Name:
-    test.loc[test.Name==i, 'Player_ID'] = uuid.uuid3(uuid.NAMESPACE_DNS, i)
+    # mapping table
+    dkmap = dkpts_df[['slug', 'name']].drop_duplicates()
+    dkmap2 = sal_df[['ID', 'Name']].drop_duplicates()
+    mappingtable = fuzzy_merge(dkmap2, dkmap, key1='Name', key2='name')
+    mappingtable.drop('ID', axis=1, inplace=True)
+    mappingtable = mappingtable.drop_duplicates()
+    test = mappingtable.merge(dkmap, left_on='matches', right_on='name', how='left')
 
-test.drop('matches', axis=1, inplace=True)
-test.columns = ['DK_Name', 'BBR_slug', 'BBR_name', 'Player_ID']
+    for i in test.Name:
+        test.loc[test.Name==i, 'Player_ID'] = uuid.uuid3(uuid.NAMESPACE_DNS, i)
 
-test.to_csv(r'data\processed\mappingtable.csv')
+    test.drop('matches', axis=1, inplace=True)
+    test.columns = ['DK_Name', 'BBR_slug', 'BBR_name', 'Player_ID']
+
+    test.to_csv(r'data\processed\mappingtable.csv')
