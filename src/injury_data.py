dcb53ee18fde599a3839a12b4509da64f8b82852
KGe001 <69283269+KGe001@users.noreply.github.com>
2020-12-29 13:55:53 -0500

    20201222-20201229 data update
    Data download for first week of season. Adjusted Injury Report download to match new timing for season (nba.com)

--- src/injury_data.py
+++ src/injury_data.py
@@ -14,21 +14,27 @@ today = today.strftime('%Y-%m-%d')
 ROOT_DIR = os.path.abspath(os.curdir)
 
 def download_nba_injuryreports(date):
-    # given a date in %Y-%m-%d string format, will try to download the injury reports that the nba publishes at 11, 2, and 5
-    url1 = 'https://ak-static.cms.nba.com/referee/injury/Injury-Report_' + date + '_11AM.pdf'
-    url2 = 'https://ak-static.cms.nba.com/referee/injury/Injury-Report_' + date + '_02PM.pdf'
-    url3 = 'https://ak-static.cms.nba.com/referee/injury/Injury-Report_' + date + '_05PM.pdf'
+    # # given a date in %Y-%m-%d string format, will try to download the injury reports that the nba publishes at 11, 2, and 5
+    # url1 = 'https://ak-static.cms.nba.com/referee/injury/Injury-Report_' + date + '_11AM.pdf'
+    # url2 = 'https://ak-static.cms.nba.com/referee/injury/Injury-Report_' + date + '_02PM.pdf'
+    # url3 = 'https://ak-static.cms.nba.com/referee/injury/Injury-Report_' + date + '_05PM.pdf'
+
+    # season updated injury report for 20-21 to 1:30, 5:30, 8:30
+    url1 = 'https://ak-static.cms.nba.com/referee/injury/Injury-Report_' + date + '_01PM.pdf'
+    url2 = 'https://ak-static.cms.nba.com/referee/injury/Injury-Report_' + date + '_05PM.pdf'
+    url3 = 'https://ak-static.cms.nba.com/referee/injury/Injury-Report_' + date + '_08PM.pdf'
+
     r1 = requests.get(url1, allow_redirects=True)
     r2 = requests.get(url2, allow_redirects=True)
     r3 = requests.get(url3, allow_redirects=True)
 
     path = ROOT_DIR + '\\data\\raw\\injury_reports'
 
-    if (r1.status_code == 200) and (os.path.exists(path + '\\' + 'Injury-Report_' + date + '_11AM.pdf') is False):
+    if (r1.status_code == 200) and (os.path.exists(path + '\\' + 'Injury-Report_' + date + '_01PM.pdf') is False):
         open(path + "\\" + 'Injury-Report_' + date + '_11AM.pdf', 'wb').write(r1.content)
-    if (r2.status_code == 200) and (os.path.exists(path + '\\' + 'Injury-Report_' + date + '_02PM.pdf') is False):
+    if (r2.status_code == 200) and (os.path.exists(path + '\\' + 'Injury-Report_' + date + '_05PM.pdf') is False):
         open(path + "\\" + 'Injury-Report_' + date + '_02PM.pdf', 'wb').write(r2.content)
-    if (r3.status_code == 200) and (os.path.exists(path + '\\' + 'Injury-Report_' + date + '_05PM.pdf') is False):
+    if (r3.status_code == 200) and (os.path.exists(path + '\\' + 'Injury-Report_' + date + '_08PM.pdf') is False):
         open(path + "\\" + 'Injury-Report_' + date + '_05PM.pdf', 'wb').write(r3.content)
 
 # converts pdf injury reports into csv files
